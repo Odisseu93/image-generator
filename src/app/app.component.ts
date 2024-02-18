ddc864918427b1de149061a9f7e347c86103d504
@Odisseu93 - Ulisses Silvério <contato@ulisses.tec.br>
2024-02-18 19:27:23 -0300

    feat:  add toasty notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ImageGeneratorService } from './shared/services/image-generator.service';
-import { MatProgressSpinnerModule  } from '@angular/material/progress-spinner'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { ToastrService } from 'ngx-toastr'
 
 @Component({
   selector: 'app-root',
@@ -23,6 +24,7 @@ export class AppComponent {
   constructor(
     private formBuilder: FormBuilder,
     private imageGeneratorService: ImageGeneratorService,
+    private toasty: ToastrService,
   ) {
     this.form = this.formBuilder.group({ imageDescription: '' });
   }
@@ -31,19 +33,24 @@ export class AppComponent {
     this.totalChar = this.form.value.imageDescription.length || 0;
   }
 
-  handleSubmit(): void {
-    this.isLoading = true;
-    this.imageGeneratorService
-      .generate({ imageDescription: this.form.value.imageDescription })
-      .subscribe({
-        next: (res: any) => {
-          this.generatedImageSrc = res[0].url;
-          this.isLoading = false;
-        },
-        error: (err: any) => {
-          console.error(err);
-          this.isLoading = false;
-        },
-      });
+  handleSubmit() {
+    if (!!!this.form.value.imageDescription) this.toasty.info("oops! Parece que você esqueceu de enviar a descrição da imagem!");
+    else {
+      this.isLoading = true;
+      this.imageGeneratorService
+        .generate({ imageDescription: this.form.value.imageDescription })
+        .subscribe({
+          next: (res: any) => {
+            this.generatedImageSrc = res[0].url;
+            this.isLoading = false;
+            this.toasty.success("uhuu! Imagem gerada com sucesso! ✅️🏁️🏁️");
+          },
+          error: (err: any) => {
+            this.toasty.error("Que estranho! Parece que alguma coisa deu errado 🤔️, tente mais tarde!");
+            console.error(err);
+            this.isLoading = false;
+          },
+        });
+    }
   }
 }
