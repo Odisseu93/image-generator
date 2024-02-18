e128a5a97fcfbcf969e83b380ba1ef0a96b10ea0
@Odisseu93 - Ulisses Silvério <contato@ulisses.tec.br>
2024-02-18 18:54:37 -0300

    feat: add HTML, code, and styling to the APP component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ImageGeneratorService } from './shared/services/image-generator.service';
+import { MatProgressSpinnerModule  } from '@angular/material/progress-spinner'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule, MatProgressSpinnerModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.less',
 })
 export class AppComponent {
-  title = 'image-generator';
+  title = 'Gerador de images';
+  form: FormGroup;
+  totalChar = 0;
+  isLoading = false;
+
+  @Output() generatedImageSrc: string = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private imageGeneratorService: ImageGeneratorService,
+  ) {
+    this.form = this.formBuilder.group({ imageDescription: '' });
+  }
+
+  countChar(): void {
+    this.totalChar = this.form.value.imageDescription.length || 0;
+  }
+
+  handleSubmit(): void {
+    this.isLoading = true;
+    this.imageGeneratorService
+      .generate({ imageDescription: this.form.value.imageDescription })
+      .subscribe({
+        next: (res: any) => {
+          this.generatedImageSrc = res[0].url;
+          this.isLoading = false;
+        },
+        error: (err: any) => {
+          console.error(err);
+          this.isLoading = false;
+        },
+      });
+  }
 }
