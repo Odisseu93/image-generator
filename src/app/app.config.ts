1868b9f72036626f37b3d1fe04b79f67f55b7adf
@Odisseu93 - Ulisses Silvério <contato@ulisses.tec.br>
2024-02-18 19:26:28 -0300

    chore: add ngx-toastr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimations  } from '@angular/platform-browser/animations'
+import { provideAnimations } from '@angular/platform-browser/animations'
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideAnimations(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideHttpClient(), provideAnimations(), provideAnimationsAsync(), provideToastr({
+    timeOut: 2000,
+    closeButton: true,
+    preventDuplicates: true,
+    progressAnimation: 'decreasing',
+  })]
 };
